e1bb0e4650f0a8141d2ed266c1eac76f1a219f43
tongjieqing <1377574698@qq.com>
2025-06-26 16:32:30 +0800

    业务-房型设置

---
+++ aspnet-core/src/HotelABP.Application.Contracts/RoomTypes/UpdataRoomTypeOrderDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelABP.RoomTypes
+{
+    public class UpdataRoomTypeOrderDto
+    {
+        public Guid Id { get; set; }
+        public int Order { get; set; }
+    }
+}
