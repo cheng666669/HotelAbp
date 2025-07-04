7ed2c44195f71dfbd8cd5e3cff4daf7e44a7ed88
1dangdang <2698997479@qq.com>
2025-07-04 21:48:01 +0800

    3211232321

---
+++ aspnet-core/src/HotelABP.Application.Contracts/Account/AccountDto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Volo.Abp.Application.Dtos;
+
+namespace HotelABP.Account
+{
+    public class AccountDto 
+    {
+
+        /// <summary>
+        /// 手机号
+        /// </summary>
+        public string Mobile { get; set; } = string.Empty;
+        /// <summary>
+        /// 昵称
+        /// </summary>
+        public string NickName { get; set; } = string.Empty;
+        /// <summary>
+        /// 性别
+        /// </summary>
+        public Gender Gender { get; set; }
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string UserName { get; set; } = string.Empty;
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string Password { get; set; } = string.Empty;
+        /// <summary>
+        /// 状态（1-正常，0-禁用）
+        /// </summary>
+        public Status Status { get; set; }
+        /// <summary>
+        /// 邮箱
+        /// </summary>
+        public string Email { get; set; } = string.Empty;
+    }
+}
