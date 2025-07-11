2953b603302dcdec909f8942cbbcad57db003edd
cheng666669 <1035107202@qq.com>
2025-07-11 19:06:55 +0800

    niu

--- aspnet-core/src/HotelABP.Domain/Customers/Balancerecord.cs
+++ aspnet-core/src/HotelABP.Domain/Customers/Balancerecord.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
 
 namespace HotelABP.Customers
 {
-    internal class Balancerecord
+    public class Balancerecord: FullAuditedEntity<Guid>
     {
+
     }
 }
